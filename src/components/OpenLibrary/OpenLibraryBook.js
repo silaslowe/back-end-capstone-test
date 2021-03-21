66cc456a083814f91ab17f1b2063a7805ed44d11
negative1skunk@gmail.com <silaslowe@gmail.com>
2021-03-20 22:20:31 -0500

    removed some old code in ol-book-display

--- src/components/OpenLibrary/OpenLibraryBook.js
+++ src/components/OpenLibrary/OpenLibraryBook.js
@@ -43,24 +43,3 @@ export const OpenLibraryBook = ({ props, book }) => {
     </>
   )
 }
-
-{
-  /* <div key={book.edition_key[0]} className="ol-book">
-<img src={`http://covers.openlibrary.org/b/olid/${book.cover_edition_key}-L.jpg` || {}} />
-<p>Title: {book.title}</p>
-<p>Author: {authors}</p>
-<button
-  onClick={() => {
-    createNewGuide({
-      author: authors,
-      coverUrl:
-        `http://covers.openlibrary.org/b/olid/${book.cover_edition_key}-L.jpg` || {},
-      title: book.title,
-      publishYear: book.first_publish_year,
-    })
-  }}
->
-  Save Book
-</button>
-</div> */
-}
